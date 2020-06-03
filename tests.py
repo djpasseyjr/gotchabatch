62c8710430c35a1377c1eb0e35094e30357c2b12
DJ Passey <djpasseyjr@gmail.com>
2020-06-03 13:11:51 -0600

    Test absolute file locations

--- tests.py
+++ tests.py
@@ -1,9 +1,11 @@
-from generate_jobs import *
+from .generate_jobs import *
 import os
 import numpy as np
+import pathlib
+DIR = str(pathlib.Path(__file__).parent.absolute())
 
 def empty_test_dir():
-    test_dir = "GeneratedJobs/test_job/"
+    test_dir = DIR + "/GeneratedJobs/test_job/"
     job_dir = test_dir + 'jobfiles/'
     save_dir = test_dir + 'savefiles/'
     for f in os.listdir(job_dir):
@@ -13,19 +15,20 @@ def empty_test_dir():
     os.rmdir(test_dir)
 
 def rm_test_bash():
-    if os.path.exists("test_job.sh"):
-        os.unlink("test_job.sh")
+    test_bash = DIR + "/test_job.sh"
+    if os.path.exists(test_bash):
+        os.unlink(test_bash)
 
 def test_job_gen():
-    if os.path.exists("GeneratedJobs/test_job"):
+    if os.path.exists(DIR + "/GeneratedJobs/test_job"):
         empty_test_dir()
     args = [(np.random.rand(5)) for i in range(95)]
     func_path = "/Users/djpassey/Code/gotchabatch/generate_jobs.py"
     func = "fake_func"
     fprefix = "test_job"
     generate_jobs(fprefix, func_path, func, args, 10, 1, 2)
-    assert len(os.listdir("GeneratedJobs/test_job/jobfiles")) == 10
-    #empty_test_dir()
+    assert len(os.listdir(DIR + "/GeneratedJobs/test_job/jobfiles")) == 10
+    empty_test_dir()
     rm_test_bash()
 
 
