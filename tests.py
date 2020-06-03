a005d64ea07ccb0f8e24b88ffcc3d8db9caa3213
DJ Passey <djpasseyjr@gmail.com>
2020-06-03 12:36:34 -0600

    Updated tests

--- tests.py
+++ tests.py
@@ -19,13 +19,13 @@ def rm_test_bash():
 def test_job_gen():
     if os.path.exists("GeneratedJobs/test_job"):
         empty_test_dir()
-    args = [(np.random.rand(5)) for i in range(100)]
-    imports = "import numpy as np"
+    args = [(np.random.rand(5)) for i in range(95)]
+    func_path = "/Users/djpassey/Code/gotchabatch/generate_jobs.py"
     func = "fake_func"
     fprefix = "test_job"
-    generate_jobs(fprefix, imports, func, args, 10, 1, 2)
+    generate_jobs(fprefix, func_path, func, args, 10, 1, 2)
     assert len(os.listdir("GeneratedJobs/test_job/jobfiles")) == 10
-    empty_test_dir()
+    #empty_test_dir()
     rm_test_bash()
 
 
